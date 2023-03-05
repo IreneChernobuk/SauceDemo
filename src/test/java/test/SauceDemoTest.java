8971bd61765937288abb49f9201d7e27536954b4
IreneChernobuk <irinka_buc@mail.ru>
2023-03-05 15:51:20 +0300

    homework 14: annotation, TestListeners, RetryAnalyzer, Parallel

--- src/test/java/test/SauceDemoTest.java
+++ src/test/java/test/SauceDemoTest.java
@@ -5,24 +5,25 @@ import org.testng.annotations.Test;
 import pompages.AccountCreatePage;
 import pompages.ProductsPage;
 import pompages.ShoppingCardPage;
+import utils.RetryAnalyzer;
 
-public class SauceDemoTest extends BaseWithFactoryTest {
+public class SauceDemoTest extends BaseWithThreadLocalTest {
     final String USERNAME = "standard_user";
     final String PASSWORD = "secret_sauce";
 
-    @Test
+    @Test(retryAnalyzer = RetryAnalyzer.class, invocationCount = 2, threadPoolSize = 2)
     public void logInTest() {
-        AccountCreatePage accountCreatePage = new AccountCreatePage(driver);
+        AccountCreatePage accountCreatePage = new AccountCreatePage(getDriver());
         accountCreatePage.openAccountCreatePage();
         accountCreatePage.inputUserName(USERNAME);
         accountCreatePage.inputPassword(PASSWORD);
         accountCreatePage.clickLogin();
-        ProductsPage productsPage = new ProductsPage(driver);
+        ProductsPage productsPage = new ProductsPage(getDriver());
         productsPage.clickAddToCard();
         String NameItem = productsPage.receiveItemName();
         String Price = productsPage.receivePrice();
         productsPage.clickShoppingCard();
-        ShoppingCardPage shoppingCardPage = new ShoppingCardPage(driver);
+        ShoppingCardPage shoppingCardPage = new ShoppingCardPage(getDriver());
         String actualPrice = shoppingCardPage.receiveActualPrice();
         String actualNameItem = shoppingCardPage.receiveNameItem();
         Assert.assertEquals(actualNameItem, NameItem, "the product name doesn't match");
